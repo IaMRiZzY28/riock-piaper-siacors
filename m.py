d2a12ef3ca2ac8f346e949aecce320d9119335e3
IaMRiZzY28 <28ragavn@gmail.com>
2024-11-04 18:12:47 +0530

    221

--- m.py
+++ m.py
@@ -1,10 +1,34 @@
 import random
-def  gucci():
-    ucci=str(input("enter your choice")).lower()
-    while ucci not in["rock","paper","scissors"]:
+def gucci():
+    uc=str(input("enter your choice")).lower()
+    while uc not in["rock","paper","scissors"]:
         print("invalid choice")
-        ucci=input("enter correct choize bro/sis").lower()
-        return ucci
+        uc=input("enter correct choize bro/sis").lower()
+        return uc
 
 def icc():
     return random.choice(["rock","paper","scissors"])
+
+def play():
+    print("welcome to rock piaper siciasor")
+    uc= gucci()
+    cc=icc()
+    print("u chose {uc}")
+    print("computer chose {cc}")
+    result=winner(uc,cc)
+    print(result)
+
+def winner(uc,cc):
+    if uc==cc:
+        return "tie!"
+    elif(uc=="rock" and cc=="scissors")or\
+        (uc=="paper" and cc=="rock")or\
+        (uc=="sicssor" and cc=="paper"):
+
+        return "u win"
+
+    else:
+        return "computer wins"
+
+if __name__=="__m__":
+    play()
